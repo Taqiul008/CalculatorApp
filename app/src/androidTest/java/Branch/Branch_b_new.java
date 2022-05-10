e096da771bbc11461425102034244dd2d207f1cc
Taqiul008 <islam1807008@stud.kuet.ac.bd>
2022-05-11 00:33:50 +0600

    Modified b_new from master

--- app/src/androidTest/java/Branch/Branch_b_new.java
+++ app/src/androidTest/java/Branch/Branch_b_new.java
@@ -3,10 +3,12 @@ package Branch;
 public class Branch_b_new {
     int roll;
     float cgpa;
+    int attendance;
 
-    public Branch_b_new(int roll, float cgpa) {
+    public Branch_b_new(int roll, float cgpa, int attendance) {
         this.roll = roll;
         this.cgpa = cgpa;
+        this.attendance = this.attendance;
     }
 
     public int getRoll() {
@@ -23,5 +25,13 @@ public class Branch_b_new {
 
     public void setCgpa(float cgpa) {
         this.cgpa = cgpa;
+    }
+
+    public int getAttendance() {
+        return attendance;
+    }
+
+    public void setAttendance(int attendance) {
+        this.attendance = attendance;
     }
 }
